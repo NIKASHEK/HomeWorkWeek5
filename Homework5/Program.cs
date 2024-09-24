4456036fa8bb30289ff275009c81bcd09a3c295e
NIKASHEK <nikashekiladze78@gmail.com>
2024-09-25 01:32:32 +0400

    Initialy program

---
+++ Homework5/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Homework5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nika Shekiladze  Home work 5
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
