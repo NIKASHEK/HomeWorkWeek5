7a3d589165540b0b9dcc32e4903734dfc6586615
NIKASHEK <nikashekiladze78@gmail.com>
2024-09-25 02:44:49 +0400

    #3

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -4,38 +4,15 @@
     {
         static void Main(string[] args)
         {
-            // Nika Shekiladze  Home work 5
-            //Console.WriteLine("Hello World!");
-
-            // 1
-            //var enteredInt = Convert.ToInt32(Console.ReadLine());
-            //if(enteredInt % 5 == 0)
-            //    Console.WriteLine("YES");
-            //else
-            //    Console.WriteLine("NO");
-
-            // 2
-            var enteredInt1 = Convert.ToInt32(Console.ReadLine());
-            var enteredInt2 = Convert.ToInt32(Console.ReadLine());
+            // 3
+            var x = Console.ReadLine();
+            var y = Console.ReadLine();
+            var someInt = x;
+            x = y;
+            y = someInt;
             Console.WriteLine();
-            Console.WriteLine(enteredInt1 + enteredInt2);
-            Console.WriteLine(enteredInt1 * enteredInt2);
-            if (enteredInt1 >= enteredInt2)
-            {
-                Console.WriteLine(enteredInt1 - enteredInt2);
-                if(enteredInt2 == 0)
-                    Console.WriteLine("Not Allowed To Divide By Zero");
-                else
-                    Console.WriteLine(enteredInt1 / enteredInt2);
-            }
-            else
-            {
-                Console.WriteLine(enteredInt2 - enteredInt1);
-                if(enteredInt1 == 0)
-                    Console.WriteLine("Not Allowed To Divide By Zero");
-                else
-                    Console.WriteLine(enteredInt2 / enteredInt1);
-            }
+            Console.WriteLine(x);
+            Console.WriteLine(y);
         }
     }
 }
--- Homework5/obj/Debug/net8.0/Homework5.AssemblyInfo.cs
+++ Homework5/obj/Debug/net8.0/Homework5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bc2495389c565a3c01ea975fd03b12b581ad272")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11da21cad0dce60c7a25b2ac2e05f558ae8936b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
