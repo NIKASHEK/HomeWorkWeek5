9bc2495389c565a3c01ea975fd03b12b581ad272
NIKASHEK <nikashekiladze78@gmail.com>
2024-09-25 02:12:16 +0400

    %5

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -5,7 +5,15 @@
         static void Main(string[] args)
         {
             // Nika Shekiladze  Home work 5
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            // 1
+            var enteredInt = Convert.ToInt32(Console.ReadLine());
+            if(enteredInt % 5 == 0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+
         }
     }
 }
